a855c5d45e2c735a273a8a8ebf49d7fd59954d2e
RajatSablok <rajat.main06@gmail.com>
2022-09-20 01:18:39 +0530

    fix: default jwt expiry config

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = Object.freeze({
   MONGO_URL: process.env.MONGO_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   PORT: process.env.PORT,
-  DEFAULT_JWT_EXPIRY: process.env.PORT,
+  DEFAULT_JWT_EXPIRY: "30d",
 });
