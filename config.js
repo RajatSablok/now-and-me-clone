e3047ae554d9eb26d84a360e7860a2b6254e749e
RajatSablok <rajat.main06@gmail.com>
2022-09-20 16:03:54 +0530

    feat: dockerized

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-  MONGO_URL: process.env.MONGO_URL,
-  JWT_SECRET: process.env.JWT_SECRET,
-  PORT: process.env.PORT,
-  DEFAULT_JWT_EXPIRY: "30d",
+  MONGO_URL: process.env.MONGO_URL || "mongodb://mongo:27017",
+  JWT_SECRET: process.env.JWT_SECRET || "0GGOC7TavNUUYW9",
+  PORT: process.env.PORT || 5000,
+  DEFAULT_JWT_EXPIRY: process.env.DEFAULT_JWT_EXPIRY || "30d",
 });
