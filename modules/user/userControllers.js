f1edffc99e27a9c47c3017a88dd6b0f20833bac4
RajatSablok <rajat.main06@gmail.com>
2022-09-20 12:27:59 +0530

    fix: separate apis for signup/login

--- modules/user/userControllers.js
+++ modules/user/userControllers.js
@@ -2,35 +2,64 @@ const userRepository = require("./userRepository");
 
 const { getJWT, getHashedPassword, compareHash } = require("../../utils");
 
-exports.auth = async (userInfo) => {
+exports.signup = async (userInfo) => {
   try {
     // Check if user exists
     const userExists = await userRepository.findUserByUsername(
       userInfo.username
     );
 
-    // Signup if user doesn't exist, else login
+    // Return error if user already exists
+    if (userExists.status) {
+      throw Error("Username taken.");
+    }
+
+    // Hash password
+    const hashedPassword = await getHashedPassword(userInfo.password);
+
+    // Save user to the database
+    const userAdded = await userRepository.addUser(
+      userInfo.username,
+      hashedPassword
+    );
+
+    // Generate token
+    const token = await getJWT({
+      userId: userAdded.data._id,
+      username: userAdded.data.username,
+      name: userAdded.data.name,
+    });
+
+    return {
+      _id: userAdded.data._id,
+      username: userAdded.data.username,
+      token,
+    };
+  } catch (err) {
+    return { status: false, err: err.toString() };
+  }
+};
+
+exports.login = async (userInfo) => {
+  try {
+    // Check if user exists
+    const userExists = await userRepository.findUserByUsername(
+      userInfo.username
+    );
+
+    // Return error if user doesn't exist
     if (!userExists.status) {
-      // Hash password
-      const hashedPassword = await getHashedPassword(userInfo.password);
+      throw Error("Auth failed");
+    }
 
-      // Save user to the database
-      const userAdded = await userRepository.addUser(
-        userInfo.username,
-        hashedPassword
-      );
+    // Check if passwords match
+    const isPasswordCorrect = await compareHash(
+      userInfo.password,
+      userExists.data.password
+    );
 
-      userExists.data = userAdded.data;
-    } else {
-      // Check if passwords match
-      const isPasswordCorrect = await compareHash(
-        userInfo.password,
-        userExists.data.password
-      );
-
-      if (!isPasswordCorrect) {
-        throw Error("Auth failed");
-      }
+    if (!isPasswordCorrect) {
+      throw Error("Auth failed");
     }
 
     // Generate token
--- modules/user/userRoutes.js
+++ modules/user/userRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const userControllers = require("./userControllers");
 const userValidators = require("./userValidatiors");
 
-router.post("/", async (req, res, next) => {
+router.post("/signup", async (req, res, next) => {
   try {
     // Validate request body
     const validated = await userValidators.auth(req);
@@ -16,7 +16,27 @@ router.post("/", async (req, res, next) => {
 
     // Follow business logic for authentication
     const userInfo = req.body;
-    const userData = await userControllers.auth(userInfo);
+    const userData = await userControllers.signup(userInfo);
+
+    return res.status(201).json(userData);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    // Validate request body
+    const validated = await userValidators.auth(req);
+    if (validated.error) {
+      const error = new Error("Invalid request format");
+      error.status = 400;
+      return next(error);
+    }
+
+    // Follow business logic for authentication
+    const userInfo = req.body;
+    const userData = await userControllers.login(userInfo);
 
     return res.status(200).json(userData);
   } catch (err) {
